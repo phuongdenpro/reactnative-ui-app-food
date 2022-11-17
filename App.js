a1918d38d1b798740f150ba4fbe68227bf3f85db
tranvannhan1911 <tranvannhan1911@gmail.com>
2022-11-17 17:12:20 +0700

    welcome screen

--- App.js
+++ App.js
@@ -1,12 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+// import LoginScreen from './src/screen/login'
+// import ForgotScreen from './src/screen/forgot'
+// import ForgotVerifyScreen from './src/screen/forgot-verify'
+// import { NativeBaseProvider } from 'native-base';
+// import HomeScreen from './src/screen/home';
+// import ChangePassScreen from './src/screen/changePass';
+// import OrderDetail from './src/components/order/order_detail';
+// import OrderDetailScreen from './src/components/order/order_detail';
+// import BarcodeScanner from './src/components/barcode/barcode_scanner';
+// import ProductDetailScreen from './src/screen/product_detail';
+// import PromotionDetailScreen from './src/screen/promotion_detail';
+import WelcomeScreen from './src/screen/welcome';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName='Welcome'
+        //   screenOptions={{
+        //     headerShown: false
+        // }}
+        >
+          <Stack.Screen name="Welcome" component={WelcomeScreen}
+            options={{ headerShown: false }} />
+
+          {/* <Stack.Screen name="Login" component={LoginScreen}
+            options={{ title: 'Đăng nhập', headerShown: false }} />
+
+          <Stack.Screen name="Forgot" component={ForgotScreen}
+            options={{ title: 'Quên mật khẩu' }} />
+
+          <Stack.Screen name="ForgotVerify" component={ForgotVerifyScreen}
+            options={{ title: 'Xác thực số điện thoại' }} />
+
+          <Stack.Screen name="ChangePassword" component={ChangePassScreen}
+            options={{ title: 'Đổi mật khẩu' }} />
+
+          <Stack.Screen name="Home" component={HomeScreen}
+            options={{ headerShown: false }} />
+
+          <Stack.Screen name="OrderDetail" component={OrderDetailScreen}
+            options={{ title: 'Chi tiết hóa đơn' }} />
+
+          <Stack.Screen name="BarcodeScanner" component={BarcodeScanner}
+            options={{ title: 'Quét mã vạch sản phẩm' }} />
+
+          <Stack.Screen name="ProductDetail" component={ProductDetailScreen}
+            options={{ title: 'Chi tiết sản phẩm' }} />
+
+          <Stack.Screen name="PromotionDetail" component={PromotionDetailScreen}
+            options={{ title: 'Khuyến mãi' }} /> */}
+
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
 
